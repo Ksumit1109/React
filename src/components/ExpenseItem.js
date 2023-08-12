7fca04fdb7e0280e0c3d1723a184aeb7c496ea63
Sumit <104936009+Ksumit1109@users.noreply.github.com>
2023-08-12 20:08:40 +0530

    Add files via upload

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem() {
+    return (
+        <h1>Expense Item 
+            <ul>
+                <li>Food Rs. 10</li>
+                <li>Petrol Rs. 100</li>
+                <li>Movies Rs. 200</li>
+            </ul>
+      </h1>
+        
+        
+    );
+  }
+  
+  export default ExpenseItem;
+  
