7dcc9373f41312985d2d6ee01126a561d192db8b
Sumit <104936009+Ksumit1109@users.noreply.github.com>
2023-08-20 22:07:03 +0530

    Updated Advance React (Adding Event Listeners-Delete button via DOM)

--- src/ExpenseDetails.js
+++ src/ExpenseDetails.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import './ExpenseDetails.css'
 
+
 const ExpenseDetails=(props)=>{
-    const deleteBtn = ()=>{
-        const parentElement = document.getElementById(`expense-container-${props.id}`)
-        // Check if the parent element exists before attempting to remove it
-        if (parentElement) {
-          parentElement.remove();
-        }
-    
-    } 
     return (
-            <div className="expense-item__description" id={`expense-container-${props.id}`}>
+            <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <h3>{props.locationOfExp}</h3>
                 <div className="expense-item__price">{props.price}</div>
-                <button onClick={deleteBtn}>Delete</button>
             </div>
     )
 }
--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -3,11 +3,21 @@ import ExpenseDetails from "./ExpenseDetails"
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 
+
 const ExpenseItem=(props)=> {
+    const deleteBtn = ()=>{
+        const parentElement = document.getElementById(`expense-container-${props.id}`)
+        // Check if the parent element exists before attempting to remove it
+        if (parentElement) {
+          parentElement.remove();
+        }
+    
+    } 
     return (
-        <div className="expense-item">
+        <div className="expense-item" id={`expense-container-${props.id}`}>
             <ExpenseDate  date={props.date}/>
             <ExpenseDetails id={props.id} title={props.title} locationOfExp={props.locationOfExp} price={props.price} />
+            <button onClick={deleteBtn}>Remove</button>
         </div>
     );
 }
