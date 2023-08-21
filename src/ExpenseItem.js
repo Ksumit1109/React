5c89fa83d45485547e2a60cf5837530fd69e0858
Sumit <104936009+Ksumit1109@users.noreply.github.com>
2023-08-21 21:08:11 +0530

    Manipulating the DOM - React States -- delBtn the value and change the title using useState

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ExpenseDetails from "./ExpenseDetails"
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 
 
 const ExpenseItem=(props)=> {
+    let exp = props.price
+    const [Expense,SetExp] = useState(exp)
+    const changeExp =()=>{
+        SetExp("100$")
+    }
+    const [removed,DoneRemoved]= useState(false)
     const deleteBtn = ()=>{
-        const parentElement = document.getElementById(`expense-container-${props.id}`)
-        // Check if the parent element exists before attempting to remove it
-        if (parentElement) {
-          parentElement.remove();
-        }
-    
+        DoneRemoved(true)
     } 
+
+    if(removed){
+        return null
+    }
+
     return (
+        // id={expense-container-${props.id}}: This line generates a unique id for each expense item's container element by appending 
+        // the ids prop to the string "expense-container-". The resulting id will look something like "expense-container-0" for the first
+        //  expense item, "expense-container-1" for the second item, and so on.
         <div className="expense-item" id={`expense-container-${props.id}`}>
             <ExpenseDate  date={props.date}/>
-            <ExpenseDetails id={props.id} title={props.title} locationOfExp={props.locationOfExp} price={props.price} />
+            <ExpenseDetails id={props.id} title={props.title} locationOfExp={props.locationOfExp} price={Expense} />
+            <button onClick={changeExp}>Change Exp</button>
             <button onClick={deleteBtn}>Remove</button>
         </div>
     );
