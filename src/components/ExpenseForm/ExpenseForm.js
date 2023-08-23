04a8ddba8aedef9e783ba5c4b436914922b65334
Sumit <104936009+Ksumit1109@users.noreply.github.com>
2023-08-23 20:52:03 +0530

    Submitting the forms

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,66 +1,59 @@
 import React, { useState } from 'react';
 const ExpenseForm = () => {
 
-    // const [title,setTitle] = useState('');
-    // const titleChng = (event)=>{
-    //     setTitle(event.target.value);
-    // }
-
-    // const [amount,setAmt] = useState('')
-    // const amtChng = (event)=>{
-    //     setAmt(event.target.value)
-    // }
-
-    // const [date,setDate] = useState('');
-    // const dateChng = (event)=>{
-    //     setDate(event.target.value)
-
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmt: '',
-        enteredDate: ''
-    })
-
-    const titleChng = (event) => {
-        setUserInput((prevState) => {
-            return { ...prevState, enteredTitle: event.target.value }
-        })
+    const [enteredTitle,setTitle] = useState('');
+    const titleChng = (event)=>{
+        setTitle(event.target.value);
     }
 
-    const amtChng = (event) => {
-        setUserInput((prevState) => {
-            return { ...prevState, enteredAmt: event.target.value }
-        })
+    const [enteredAmt,setAmt] = useState('')
+    const amtChng = (event)=>{
+        setAmt(event.target.value)
     }
 
-    const dateChng = (event) => {
-        setUserInput((prevState) => {
-            return { ...prevState, enteredDate: event.target.value }
-        })
+    const [enteredDate,setDate] = useState('');
+    const dateChng = (event)=>{
+        setDate(event.target.value)
     }
 
-    // Event handler for form submission
-    const submitbtn = (event) => {
-        event.preventDefault(); // Prevent the form from submitting and refreshing the page
-        console.log(userInput); // Log the user input to the console
-    };
+    const submitHandler = (event)=>{
+        event.preventDefault()
+
+        const expData = {
+            title : enteredTitle,
+            amount : enteredAmt,
+            date : new Date(enteredDate)
+        }
+        console.log(expData)
+    }
+        
+
+    // // Event handler for form submission
+    // const submitbtn = (event) => {
+    //     event.preventDefault(); // Prevent the form from submitting and refreshing the page
+    //     console.log(userInput); // Log the user input to the console
+    // };
 
 
 
 
     return (
         <div>
-            <form id="Form">
+            <form id="Form" onSubmit={submitHandler}>
                 <label class="Title">Title</label>
                 <input type="text" onChange={titleChng} />
                 <label class="Amount">Amount</label>
                 <input type="text" onChange={amtChng} />
                 <label class="Date">Date</label>
                 <input type="date" onChange={dateChng} />
+                <button type='submit'>Submit</button>
             </form>
-            <button onClick={submitbtn}>Submit</button>
         </div>
     )
 }
 
 export default ExpenseForm
+
+
+
+
